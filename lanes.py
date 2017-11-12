0d95ec4d69d365f7eda4b7ccacdd493ba5b2a7ed
Alexandre Allaire <aallaire@Alexandres-iMac.fios-router.home>
2017-11-11 20:06:33 -0500

    Add lane curvature and video pipeline.

--- lanes.py
+++ lanes.py
@@ -1,5 +1,13 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+
+LANE_LENGTH = 30
+LANE_WIDTH = 3.7
+
+LANE_LENGTH_PIXELS = 720
+LANE_WIDTH_PIXELS = 620
+LANE_EPSILON = 50
 
 # Return img with lane visualizations overlayed.
 def visualize_lanes(img, window_height, window_width, left_x, right_x, left_poly, right_poly, out_file):
@@ -16,13 +24,28 @@ def visualize_lanes(img, window_height, window_width, left_x, right_x, left_poly
         windows = set_pixels(windows, i, right_x[num_points - i - 1])
 
     canvas = np.zeros_like(img)
-    windows = np.array(cv2.merge((canvas, canvas, windows)), np.uint8)
+    windows = np.array(cv2.merge((windows, canvas, canvas)), np.uint8)
     color_img = np.array(cv2.merge((img, img, img)), np.uint8)
     out_img = cv2.addWeighted(color_img, 1, windows, 0.5, 0.0)
-    cv2.imwrite(out_file, out_img)
 
-eps = 0.001
+    y = np.linspace(0, out_img.shape[0]-1, out_img.shape[0])
+    px_l = np.polyval(left_poly, y)
+    px_r = np.polyval(right_poly, y)
 
+    curve_left, curve_right = lane_curvature(left_poly, right_poly)
+
+    plt.imshow(out_img)
+    plt.plot(px_l, y, color='yellow')
+    plt.plot(px_r, y, color='yellow')
+    plt.title('Left curvature: %.2fm, Right curvature: %.2fm' % (curve_left, curve_right))
+    plt.savefig(out_file)
+    plt.gcf().clear()
+
+#
+# Helper used in fit_poly below. When a patch has no pixels set, np.argmax returns
+# the first element in the array. This biases the dotted lanes to curve left. Use the midpoint
+# of the array instead.
+#
 def middle_max(l):
     m = np.argmax(l)
     maxes = np.argwhere(l == m).flatten().tolist()
@@ -30,65 +53,79 @@ def middle_max(l):
 
 #
 # Takes a binary image containing a top-down view of the lanes, and returns
-# a polynomial fit for each lane. Optionally writes an image with the fit overlayed.
+# a polynomial fit for each lane. Optionally writes an image with a visualization of the fit overlayed.
 #
-def fit_poly(img, out_file='', window_width=30, window_height=30, search_radius=30):
+# Implemented by convoluting a window across a horizontal slice of the image to find a patch with the most
+# pixels set. This patch is considered to be where the lane is.
+#
+def fit_poly(img, prev_left=None, prev_right=None, out_file='', window_width=40, window_height=80, search_radius=LANE_EPSILON):
 
     left_x = []
     right_x = []
     window = np.ones(window_width)
     half_image = img.shape[1]//2
+    full_image = img.shape[1]
     half_window = window_width//2
-
-    # Run first convolution across entire botton of image to find inital lines.
-    # Left half first.
     ny = img.shape[0] // window_height
 
-    hist = np.sum(img[window_height*(ny-1):, :half_image], axis=0)
-    conv = np.convolve(hist, window)
+    for i in reversed(range(ny)):
 
-    # With 'full' padding, convolution indices correspond to right side of window.
-    left = max(np.argmax(conv) - half_window, 0)
-
-    hist = np.sum(img[window_height*(ny-1):, half_image:], axis=0)
-    conv = np.convolve(hist, window)
-
-    right = min(np.argmax(conv) - half_window + half_image, img.shape[1])
-
-    left_x.append(left)
-    right_x.append(right)
-
-    for i in reversed(range(ny-1)):
-
-        hist = np.sum(img[window_height*i:window_height*(i+1), :half_image], axis=0)
+        hist = np.sum(img[window_height*i:window_height*(i+1),...], axis=0)
         conv = np.convolve(hist, window)
 
-        left_search_endpoint = max(left + half_window - search_radius, 0)
-        right_search_endpoint = min(left + half_window + search_radius, half_image)
+        left_search_endpoint = 0
+        right_search_endpoint = half_image
+
+        if prev_left:
+            left_search_endpoint = max(prev_left + half_window - search_radius, 0)
+            right_search_endpoint = min(prev_left + half_window + search_radius, full_image)
 
         conv_max = middle_max(conv[left_search_endpoint:right_search_endpoint])
-        left = max(conv_max + left_search_endpoint - half_window, 0)
+        left = min(max(conv_max + left_search_endpoint - half_window, 0), full_image)
 
-        hist = np.sum(img[window_height*i:window_height*(i+1), half_image:], axis=0)
-        conv = np.convolve(hist, window)
+        left_search_endpoint = half_image
+        right_search_endpoint = len(conv)
 
-        # Need to translate right into [0, half_image].
-        left_search_endpoint = max(right + half_window - search_radius - half_image , 0)
-        right_search_endpoint = min(right + half_window + search_radius - half_image, half_image)
+        if prev_right:
+            left_search_endpoint = max(prev_right + half_window - search_radius, 0)
+            right_search_endpoint = min(prev_right + half_window + search_radius, full_image)
 
         conv_max = middle_max(conv[left_search_endpoint:right_search_endpoint])
-        right = min(conv_max + left_search_endpoint - half_window + half_image, img.shape[1])
+        right = max(min(conv_max + left_search_endpoint - half_window, full_image), 0)
 
         left_x.append(left)
         right_x.append(right)
 
+        prev_left = left
+        prev_right = right
+
     y = np.linspace(img.shape[0], window_height, (img.shape[0] // window_height))
 
     left_poly = np.polyfit(y, left_x, 2)
-    right_poly = np.polyfit(y, right_x, 2) 
+    right_poly = np.polyfit(y, right_x, 2)
 
     if out_file != '':
         visualize_lanes(img, window_height, window_width, left_x, right_x, left_poly, right_poly, out_file)
 
 
-    return left_poly, right_poly
+    return y, left_x, right_x, left_poly, right_poly
+
+# Return radius of curvature in meters.
+# Maps pixels to meters using a lane size which is probably off somewhat.
+def lane_curvature(left_poly, right_poly):
+    m_per_y = LANE_LENGTH / LANE_LENGTH_PIXELS
+    m_per_x = LANE_WIDTH / LANE_WIDTH_PIXELS
+
+    y = np.linspace(0, 719, num=720)
+
+    left_x = np.polyval(left_poly, y)
+    right_x = np.polyval(right_poly, y)
+
+    left_poly = np.polyfit(y * m_per_y, left_x * m_per_x, 2)
+    right_poly = np.polyfit(y * m_per_y, right_x * m_per_x, 2)
+
+    curve_left = ((1 + (2 * left_poly[0] * 720 * m_per_y + left_poly[1])**2)**1.5) / np.absolute(2 * left_poly[0])
+    curve_right = ((1 + (2 * right_poly[0] * 720 * m_per_y + right_poly[1])**2)**1.5) / np.absolute(2 * right_poly[0])
+
+    return curve_left, curve_right
+
--- main.py
+++ main.py
@@ -1,66 +1,171 @@
+import argparse
 import glob
 import cv2
 import numpy as np
 from calibration import *
 from transform import *
 from lanes import *
+from moviepy.editor import VideoFileClip
+from IPython.display import HTML
 
-def undistort_test(camera):
-    i = 0
-    # Undistort a few test images.
-    for file in glob.glob('./camera_cal/test_image*.jpg'):
-        img = cv2.imread(file)
+def undistort(camera, images):
+    undistorted = []
+    for img in images:
         img = camera.undistort(img)
+        undistorted.append(img)
 
-        cv2.imwrite('./output_images/undistorted{}.jpg'.format(i), img)
+    return undistorted
 
-        i += 1
+def bgr_to_hls(images):
+    hls = []
 
-def threshold_test(camera):
-    i = 0
-    images = []
-    for file in glob.glob('test_images/test*.jpg') + glob.glob('test_images/straight_lines*.jpg'):
-        img = cv2.imread(file)
-        img = camera.undistort(img)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
+    for img in images:
+        hls.append(cv2.cvtColor(img, cv2.COLOR_BGR2HLS))
+
+    return hls
+
+def threshold(images, dstack=False):
+    dstacked = []
+    thresholded = []
+
+    for img in images:
 
         grad = gradient_threshold(img, (0.2, 1.0), (0.7, 1.3))
         channel = channel_threshold(img, (150, 255))
         combined = grad | channel
 
-        cv2.imwrite('./output_images/dstacked{}.jpg'.format(i), np.dstack((np.zeros_like(combined), grad, channel)))
-        cv2.imwrite('./output_images/binary{}.jpg'.format(i), combined)
-        images.append(combined)
+        thresholded.append(combined)
 
-        i += 1
+        if dstack:
+            dstacked.append(np.dstack((np.zeros_like(combined), grad, channel)))
 
-    return images
+    return thresholded, dstacked
 
-def warp_test(images):
+def perspective_transform(images):
     warped = []
-    i = 0
     for img in images:
         w = warp(img)
-        cv2.imwrite('./output_images/warped{}.jpg'.format(i), w)
         warped.append(w)
-
-        i += 1
 
     return warped
 
-def find_lanes_test(images):
+def find_lanes(images, write=False):
     i = 0
+    polys = []
     for img in images:
-        fit_poly(img, './output_images/lanes{}.jpg'.format(i))
+        polys.append(fit_poly(img, None, None, './output_images/lanes{}.jpg'.format(i) if write else ''))
         i += 1
 
-if __name__ == '__main__':
+    return polys
+
+def draw_lanes(images, polys):
+
+    if (len(images) != len(polys)):
+        raise Exception('Number of images and polynomials differ!')
+
+    with_lanes = []
+
+    for i in range(len(images)):
+        img = images[i]
+        canvas = np.zeros_like(img, dtype=np.uint8)
+
+        y, x_l, x_r, _, __ = polys[i]
+        left = np.array([np.transpose(np.vstack([x_l, y]))])
+        right = np.array([np.flipud(np.transpose(np.vstack([x_r, y])))])
+        points = np.hstack((left, right))
+
+        cv2.fillPoly(canvas, np.int32(points), (0,255, 0))
+
+        canvas = unwarp(canvas)
+        with_lanes.append(cv2.addWeighted(img, 1, canvas, 0.3, 0))
+
+    return with_lanes
+
+def write_images(images, prefix):
+    i = 0
+    for img in images:
+        cv2.imwrite('./output_images/' + prefix + '{}'.format(i) + '.jpg', img)
+        i += 1
+
+def test():
     camera = calibrate_camera()
 
-    undistort_test(camera)
-    images = threshold_test(camera)
-    warped = warp_test(images)
-    find_lanes_test(warped)
+    images = []
+    for file in glob.glob('test_images/test*.jpg') + glob.glob('test_images/straight_lines*.jpg'):
+        images.append(cv2.imread(file))
+
+    undistorted = undistort(camera, images)
+    write_images(undistorted, 'undistorted')
+
+    hls = bgr_to_hls(undistorted)
+    binary, dstacked = threshold(hls, True)
+
+    write_images(binary, 'binary')
+    write_images(dstacked, 'dstack')
+
+    warped = perspective_transform(binary)
+    write_images(warped, 'warped')
+
+    polys = find_lanes(warped, True)
+
+    final_images = draw_lanes(images, polys)
+
+    write_images(final_images, 'final')
+
+# Smoothing factor.
+λ = 0.3
+
+prev_l = []
+prev_r = []
+
+def process_image(img):
+
+    global prev_l
+    global prev_r
+
+    hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+    fit = fit_poly(perspective_transform(threshold([hls])[0])[0], None if len(prev_l) == 0 else prev_l[0], None if len(prev_r) == 0 else prev_r[0])
+    y, x_l, x_r, _, __ = fit
+
+    # If the lane is an outlier, use previous lanes for drawing.
+    if np.absolute(np.absolute(x_l[0] - x_r[0]) - LANE_WIDTH_PIXELS) > LANE_EPSILON:
+        if (len(prev_l) > 0) and len(prev_r) > 0:
+            return draw_lanes([img], [(y, prev_l, prev_r, _, __)])[0]
+        else:
+            return img
+
+    if len(prev_l) > 0 and len(prev_r) > 0:
+        x_l = np.int32(λ * np.float64(x_l) + (1 - λ) * np.float64(prev_l))
+        x_r = np.int32(λ * np.float64(x_r) + (1 - λ) * np.float64(prev_r))
+
+    prev_l = x_l
+    prev_r = x_r
+    return draw_lanes([img], [(y, x_l, x_r, _, __)])[0]
+
+def video(video, out_file):
+
+    camera = calibrate_camera()
+
+    process = lambda img : process_image(camera.undistort(img))
+
+    video = VideoFileClip(video)
+    output = video.fl_image(process)
+    output.write_videofile(out_file, audio=False)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Lane Finding')
+    parser.add_argument('-t', '--test', action="store_true", default='', help='Test algorithms on images in test_images directory.')
+    parser.add_argument('-v', '--video', type=str, default='', help='Run video pipeline on specified video clip.')
+    parser.add_argument('-o', '--output', type=str, default='output.mp4', help='Name of processed video file.')
+    args = parser.parse_args()
+
+    if args.test:
+        test()
+
+    elif args.video:
+        video(args.video, args.output)
+    else:
+        parser.print_help()
 
 
 
